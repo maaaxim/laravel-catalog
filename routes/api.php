7de8db525068b24c2d65514f2b3af9404c4016f2
Maxim Loboda <maxim.loboda@gmail.com>
2019-05-28 09:22:30 +0300

    setup passport (need remote changes)

--- routes/api.php
+++ routes/api.php
@@ -13,13 +13,23 @@ use Illuminate\Http\Request;
 |
 */
 
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+//
+//Route::apiResource('categories', 'CategoryController')->only(['index', 'store', 'show', 'update', 'delete']);
+//Route::get('categories/{category}/products', 'CategoryController@products');
+//
+//Route::apiResource('products', 'ProductController')->only(['index', 'store', 'show', 'update', 'delete']);
+//Route::get('products/{product}/sell', 'ProductController@sell');
+//Route::get('products/{product}/return', 'ProductController@return');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+	return $request->user();
 });
 
-Route::apiResource('categories', 'CategoryController')->only(['index', 'store', 'show', 'update', 'delete']);
-Route::get('categories/{category}/products', 'CategoryController@products');
-
-Route::apiResource('products', 'ProductController')->only(['index', 'store', 'show', 'update', 'delete']);
-Route::get('products/{product}/sell', 'ProductController@sell');
-Route::get('products/{product}/return', 'ProductController@return');
+Route::post('login', 'API\UserController@login');
+Route::post('register', 'API\UserController@register');
+Route::group(['middleware' => 'auth:api'], function(){
+	Route::post('details', 'API\UserController@details');
+});
