176e341e04a36889459392b4559773201127e612
Maxim Loboda <loboda@aeroidea.ru>
2019-05-30 21:04:53 +0300

    categories tests ok

--- tests/Feature/Http/CategoryControllerTest.php
+++ tests/Feature/Http/CategoryControllerTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Feature\Http\Controllers;
 
 use App\Category;
+use App\Product;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 use \App\User;
 
@@ -27,7 +29,7 @@ class CategoryControllerTest extends TestCase
 
 	public function test_can_get_categories()
 	{
-		$this->category = factory(Category::class, 10)->create();
+		factory(Category::class, 10)->create();
 		$response = $this->json('GET', '/api/categories', []);
 		$response->assertStatus(200);
 		$response->assertJsonStructure([
@@ -106,17 +108,51 @@ class CategoryControllerTest extends TestCase
 	public function test_authorized_delete_category()
 	{
 		$category = factory(Category::class)->create();
-		$response = $this->json('DELETE', '/api/categories/' . $category->id, [	]);
+		$response = $this->json('DELETE', '/api/categories/' . $category->id, []);
 		$response->assertStatus(401);
 	}
-//
-//	public function test_can_get_product_list()
-//	{
-//
-//	}
-//
-//	public function test_error_get_product_list_wrong_product()
-//	{
-//
-//	}
+
+	public function test_can_get_product_list()
+	{
+		$category = factory(Category::class)->create();
+		for($i = 0; $i < 10; $i++){
+			DB::table('category_product')->insert(
+				[
+					'product_id' => factory(Product::class)->create()->id,
+					'category_id' => $category->id,
+				]
+			);
+		}
+		$response = $this->json(
+			'GET',
+			'/api/categories/' . $category->id . '/products',
+			[]
+		);
+		$response->assertStatus(200);
+		$response->assertJsonStructure([
+			["id", "name"]
+		]);
+	}
+
+	public function test_error_get_product_list_wrong_category()
+	{
+		$response = $this->json(
+			'GET',
+			'/api/categories/100500/products',
+			[]
+		);
+		$response->assertStatus(404);
+	}
+
+	public function test_error_get_product_list_empty_category()
+	{
+		$category = factory(Category::class)->create();
+		$response = $this->json(
+			'GET',
+			'/api/categories/' . $category->id . '/products',
+			[]
+		);
+		$response->assertStatus(200);
+		$response->assertJsonStructure([]);
+	}
 }
