77363cf2e4baa290f6eadd5a2fad528226f1f0ff
Maxim Loboda <maxim.loboda@gmail.com>
2019-06-03 07:56:47 +0300

    amount fix

--- database/migrations/2019_05_25_141846_create_products_table.php
+++ database/migrations/2019_05_25_141846_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
 			$table->string('name');
-			$table->bigInteger('amount');
+			$table->bigInteger('amount')->default(0);
             $table->timestamps();
         });
     }
